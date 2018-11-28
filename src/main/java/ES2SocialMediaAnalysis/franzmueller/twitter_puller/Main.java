f252be6097151a26b67bf0f7fe3b7d5c6a30a58b
franzmueller <franzmueller@users.noreply.github.com>
2018-11-28 15:18:29 +0100

    Introducing RichUser

--- src/main/java/ES2SocialMediaAnalysis/franzmueller/twitter_puller/Main.java
+++ src/main/java/ES2SocialMediaAnalysis/franzmueller/twitter_puller/Main.java
@@ -1,17 +1,25 @@
 package ES2SocialMediaAnalysis.franzmueller.twitter_puller;
 
-import ES2SocialMediaAnalysis.franzmueller.twitter_puller.util.ReachCoefficient;
+import ES2SocialMediaAnalysis.franzmueller.twitter_puller.RichTwitterObjects.RichUser;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
+/**
+ * @author franzmueller
+ */
 public class Main {
-    public static void main(String[] args) throws TwitterException {
+    public static void main(String[] args) throws TwitterException, InterruptedException {
         Twitter twitter = TwitterFactory.getSingleton();
 
-        System.out.println("franzmueller46 has " + ReachCoefficient.getReachCoefficient("franzmueller46", twitter) + " RC");
-        System.out.println("janboehm has " + ReachCoefficient.getReachCoefficient("janboehm", twitter) + " RC");
-        System.out.println("nike has " + ReachCoefficient.getReachCoefficient("nike", twitter) + " RC");
+        RichUser franzmueller46 = new RichUser("franzmueller46", twitter);
+
+        System.out.println("franzmueller46 has " + franzmueller46.getReachCoefficient() + " rc");
+
+        long[] followers = franzmueller46.getFollowers();
+
+        System.out.println("followers: ");
+        for (long l : followers) System.out.println(l);
     }
 }
 
---
+++ src/main/java/ES2SocialMediaAnalysis/franzmueller/twitter_puller/RichTwitterObjects/RichUser.java
@@ -0,0 +1,66 @@
+package ES2SocialMediaAnalysis.franzmueller.twitter_puller.RichTwitterObjects;
+
+import ES2SocialMediaAnalysis.franzmueller.twitter_puller.util.ReachCoefficient;
+import org.apache.commons.lang3.ArrayUtils;
+import twitter4j.IDs;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.User;
+
+public class RichUser {
+    protected final User user;
+    protected final Twitter twitter;
+    protected double reachCoefficient;
+    protected long userID; //I don't know if the user object saves this field or asks for it every time.
+    protected long[] followerIDs;
+
+
+    public RichUser(String userName, Twitter twitter) throws TwitterException {
+        user = twitter.users().showUser(userName);
+        userID = user.getId();
+        this.twitter = twitter;
+    }
+
+    public RichUser(long userID, Twitter twitter) throws TwitterException {
+        user = twitter.users().showUser(userID);
+        this.userID = userID;
+        this.twitter = twitter;
+    }
+
+    public User getUser() throws TwitterException {
+        return twitter.showUser(user.getId()); //creates a copy
+    }
+
+    public double getReachCoefficient() throws TwitterException {
+        if (reachCoefficient == 0) {
+            return reachCoefficient = ReachCoefficient.getReachCoefficient(user, twitter);
+        }
+        return reachCoefficient;
+    }
+
+    public long[] getFollowers() throws InterruptedException, TwitterException {
+        if (followerIDs == null) {
+            long cursor = -1;
+            boolean runGood;
+            IDs ids = null;
+            do {
+                runGood = false;
+                try {
+                    ids = twitter.getFollowersIDs(userID, cursor);
+                    long[] followerIDs1 = ids.getIDs();
+                    followerIDs = ArrayUtils.addAll(followerIDs, followerIDs1);
+                    runGood = true;
+                } catch (TwitterException e) {
+                    if (e.getMessage().contains("Rate")) {
+                        System.out.println("Rate limit exceeded while retrieving followers. Will sleep for 120s.");
+                        Thread.sleep(120000);
+                    } else
+                        throw new TwitterException(e.getMessage());
+                }
+            } while (!runGood && ((cursor = ids.getNextCursor()) != 0)); //Takes advantage of lazy-evaluation.
+
+        }
+        return followerIDs;
+
+    }
+}
